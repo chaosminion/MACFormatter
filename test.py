02e0e1f42aa42fa28f7a4c3d88c9877df6b1c706
chaosminion <dittmer.matthew@gmail.com>
2013-04-21 17:44:23 -0500

    I updated the code to fix many pylint errors.  There is still room for improvement.

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+'''
+The following are tests for macformatter.py
+'''
 import unittest
 from macformatter import *
 
@@ -5,30 +9,39 @@ class TestMACFormatConverterResult(unittest.TestCase):
     '''
     The purpose of these test is to test the logic of format conversion.
     '''
-
     def test_address_with_no_delimiters(self):
-
+        '''
+        Convert MAC address with no delimiters
+        '''
         self.result = ['001122334455']
         for test in self.result:
-            MACAddress = address_with_no_delimiters(test)
-            self.assertIs(len(MACAddress), 17,
+            mac_address = address_with_no_delimiters(test)
+            self.assertIs(len(mac_address), 17,
                           "The address is not 17 characters.")
-            print("address_with_no_delimiters", MACAddress)
+            print("address_with_no_delimiters", mac_address)
 
     def test_address_with_periods(self):
-
+        '''
+        Remote the two period delimiters and seperate octect pairs
+        with colons.
+        '''
         self.result = ('0011.2233.4455', 'abcd.ef01.0123')
         for test in self.result:
-            MACAddress = address_with_periods(test)
-            print('address_with_periods', MACAddress)
-            self.assertIs(MACAddress[2], ':', 'Incorrect delimiter detected!')
+            mac_address = address_with_periods(test)
+            print('address_with_periods', mac_address)
+            self.assertIs(mac_address[2], ':', 'Incorrect delimiter detected!')
 
     def test_convert(self):
+        '''
+        Test the input?  This method may have to be reconsidered.
+        Is this needed?
+        '''
         self.result = ('0011223344556', '01234567890123456')
-        for unformatedMACAddress in self.result:
-            print('test_convert', unformatedMACAddress)
-            self.assertFalse((len(unformatedMACAddress) > 18) and
-                             (self.answerLabel['text'] != ''), 'The input is too long!')
+        for unformatedmac_address in self.result:
+            print('test_convert', unformatedmac_address)
+            self.assertFalse((len(unformatedmac_address) > 18) and
+                             (self.answerLabel['text'] != ''), 'The\
+                             input is too long!')
 
     def test_five_delimiters(self):
         '''
