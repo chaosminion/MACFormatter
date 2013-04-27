chaosminion/MACFormatter

commit cca4261b2acf5b011d027b8396dd93a989b42d1e

Added test for period delimited input.

test.py
@@ -47,8 +47,8 @@ class TestMACFormatConverterResult(unittest.TestCase):
         '''
         Convert any delimiter including spaces to colins.
         '''
-        self.test = ("00 11 22 33 44 55", "00*11*22*33*44*55", \
-                     "01.23.45.67.89.10")
+##        self.test = ("00 11 22 33 44 55", "00*11*22*33*44*55", \
+##                     "01.23.45.67.89.10")
 
 ##        import pdb; pdb.set_trace()
 

commit cfb391c3adc6407d68b5cc48857051eb9797b787

Fixed bug introduced with commit cca4261b.  Part of the test was commented out.

test.py
@@ -47,8 +47,8 @@ class TestMACFormatConverterResult(unittest.TestCase):
         '''
         Convert any delimiter including spaces to colins.
         '''
-##        self.test = ("00 11 22 33 44 55", "00*11*22*33*44*55", \
-##                     "01.23.45.67.89.10")
+        self.test = ("00 11 22 33 44 55", "00*11*22*33*44*55", \
+                     "01.23.45.67.89.10")
 
 ##        import pdb; pdb.set_trace()
 