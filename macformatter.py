58241bc06ae32a7f026ac3b50c4da5ff9e200855
chaosminion <dittmer.matthew@gmail.com>
2013-05-16 23:03:09 -0500

    Add showinfo for license info

--- macformatter.py
+++ macformatter.py
@@ -56,13 +56,30 @@ class GUI(object):
             '''
             Show the user license and contact information.
             '''
-            
+            ttk.tkMessageBox.showinfo("License", "This program is free software; \
+                                      you can redistribute it and/or modify it under \
+                                      the terms of the GNU General Public \
+                                      License as published by the Free \
+                                      Software Foundation; either version 2 \
+                                      of the License.  This program is \
+                                      distributed in the hope that it will \
+                                      be useful, but WITHOUT ANY WARRANTY; \
+                                      without even the implied warranty of \
+                                      MERCHANTABIL-ITY or FITNESS FOR A \
+                                      PARTICULAR PURPOSE. See the GNU General \
+                                      Public License for more details.  \
+                                      You should have received a copy of the \
+                                      GNU General Public License along \
+                                      with this program; if not, write to \
+                                      the Free Software Foundation, Inc., 51\
+                                      Franklin Street, Fifth Floor, Boston, MA \
+                                      02110-1301, USA.")
         
         menu = Menu(my_parent)
         my_parent.config(menu=menu)
 
-        filemenu = Menu(menu)
-        menu.add_cascade(label="Help", menu=filemenu)
+        helpmenu = Menu(menu)
+        menu.add_cascade(label="Help", menu=helpmenu)
         menu.add_command(label="About", command=license_window)
 
     def convert(self):
