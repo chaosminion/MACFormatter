cdb95341004715a67f4674de0b36bc7eca7ad01f
chaosminion <dittmer.matthew@gmail.com>
2013-05-20 22:26:58 -0500

    Shortend the license text.  This fixes issue #4.

--- macformatter.py
+++ macformatter.py
@@ -52,24 +52,8 @@ class GUI(object):
             '''
             Show the user license and contact information.
             '''
-            messagebox.showinfo("License", "This program is free software; \
-                                      you can redistribute it and/or modify it under \
-                                      the terms of the GNU General Public \
-                                      License as published by the Free \
-                                      Software Foundation; either version 2 \
-                                      of the License.  This program is \
-                                      distributed in the hope that it will \
-                                      be useful, but WITHOUT ANY WARRANTY; \
-                                      without even the implied warranty of \
-                                      MERCHANTABIL-ITY or FITNESS FOR A \
-                                      PARTICULAR PURPOSE. See the GNU General \
-                                      Public License for more details.  \
-                                      You should have received a copy of the \
-                                      GNU General Public License along \
-                                      with this program; if not, write to \
-                                      the Free Software Foundation, Inc., 51\
-                                      Franklin Street, Fifth Floor, Boston, MA \
-                                      02110-1301, USA.")
+            #Breaking up this line impacts the formatting
+            messagebox.showinfo("License", "This software is released under the GLPv2.  Please report bugs to dittmer.matthew@gmail.com")
         
         menu = Menu(my_parent)
         my_parent.config(menu=menu)
